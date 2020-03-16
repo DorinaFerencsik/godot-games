b33259fb7c498d027687ea58a3f868fe046d3ea8
Dorina Ferencsik <dorina.ferencsik@gmail.com>
2020-03-16 22:15:41 +0100

    2D Dodge - Mob animations, size adjustment

--- 2D Dodge/Mob.gd
+++ 2D Dodge/Mob.gd
@@ -2,12 +2,13 @@ extends RigidBody2D
 
 export var min_speed = 150
 export var max_speed = 250
-var mob_types = ["swim"]
+var mob_types = ["swim", "walk", "fly"]
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
+	$AnimatedSprite.play()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
