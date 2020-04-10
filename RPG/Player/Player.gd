6500f3e745403c03a7c94a0b2367964994fcdb0d
Dorina Ferencsik <dorina.ferencsik@gmail.com>
2020-04-10 13:08:23 +0200

    RPG - Dirt and wall TileMaps, unique wall, small fixes

--- RPG/Player/Player.gd
+++ RPG/Player/Player.gd
@@ -6,21 +6,25 @@ const FRACTION = 500
 
 var velocity = Vector2.ZERO
 var facing = Vector2()
+var screen_size
 
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 
 
+func _ready():
+	screen_size = get_viewport_rect().size
+
+
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
-	
+
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
-	
+
 	if input_vector != Vector2.ZERO:
-	#	prefix = ANIM_RUN_PREFIX
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
 		animationState.travel("Run")
@@ -30,4 +34,6 @@ func _physics_process(delta):
 		velocity = velocity.move_toward(Vector2.ZERO, FRACTION * delta)
 	
 	velocity = move_and_slide(velocity)
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
 
